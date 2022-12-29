6a9421595b12ad0067ea553fd2347b0d550756b4
Steven Stanfield <stanfield@scarecrowtech.com>
2022-12-29 05:34:34 +0000

    Added a tokio async wrapper to teh workspace.

---
+++ tokio/src/lib.rs
@@ -0,0 +1,293 @@
+use std::error::Error;
+use dashmap::DashMap;
+use sldb_core::db::{DbBytes, DbCore, DbKey};
+use sldb_core::db_config::DbConfig;
+use sldb_core::db_raw_iter::DbRawIter;
+use sldb_core::error::{FetchError, LoadHeaderError, OpenError, ReadKeyError};
+use sldb_core::fxhasher::FxHasher;
+use std::fmt::Debug;
+use std::hash::{BuildHasher, BuildHasherDefault, Hash};
+use std::{fmt, io};
+use std::sync::Arc;
+use sldb_core::error::flush::FlushError;
+use sldb_core::error::insert::InsertError;
+use tokio::sync::*;
+
+pub struct AsyncDb<K, V, const KSIZE: u16, S = BuildHasherDefault<FxHasher>>
+where
+    K: Send + Sync + Eq + Hash + DbKey<KSIZE> + DbBytes<K> + Clone + 'static,
+    V: Send + Sync + Debug + DbBytes<V> + Clone + 'static,
+    S: Send + Sync + Clone + BuildHasher + Default + 'static,
+{
+    db_read: Arc<tokio::sync::Mutex<DbCore<K, V, KSIZE, S>>>, //DbMt<K, V, KSIZE, S>,
+    write_cache: Arc<DashMap<K, V>>,
+    insert_tx: mpsc::Sender<InsertCommand<K, KSIZE>>,
+    insert_thread: Option<std::thread::JoinHandle<()>>,
+}
+
+impl<K, V, const KSIZE: u16, S> Drop for AsyncDb<K, V, KSIZE, S>
+where
+    K: Send + Sync + Eq + Hash + DbKey<KSIZE> + DbBytes<K> + Clone + 'static,
+    V: Send + Sync + Debug + DbBytes<V> + Clone + 'static,
+    S: Send + Sync + Clone + BuildHasher + Default + 'static,
+{
+    fn drop(&mut self) {
+        let _ = self.insert_tx.try_send(InsertCommand::Done);
+        if let Some(db_thread) = self.insert_thread.take() {
+            let _ = db_thread.join();
+        }
+    }
+}
+
+impl<K, V, const KSIZE: u16, S> AsyncDb<K, V, KSIZE, S>
+where
+    K: Send + Sync + Eq + Hash + DbKey<KSIZE> + DbBytes<K> + Clone + 'static,
+    V: Send + Sync + Debug + DbBytes<V> + Clone + 'static,
+    S: Send + Sync + Clone + BuildHasher + Default + 'static,
+{
+    /// Open a new or reopen an existing database.
+    pub fn open(config: DbConfig) -> Result<Self, OpenError> {
+        let (insert_tx, insert_rx) = mpsc::channel(10_000);
+        // Open the write db (passed to the insert thread).
+        let db = DbCore::open(config.clone())?;
+        // Open a read only DB for fetching, reduces locks.
+        let db_read = Arc::new(tokio::sync::Mutex::new(DbCore::open(config.read_only())?));
+        let write_cache: Arc<DashMap<K, V>> = Arc::new(DashMap::new());
+        let write_cache_clone = write_cache.clone();
+        let db_read_clone = db_read.clone();
+        let db_thread = std::thread::spawn(move || {
+            Self::insert_thread(db, db_read_clone, write_cache_clone, insert_rx)
+        });
+        Ok(Self {
+            db_read,
+            write_cache,
+            insert_tx,
+            insert_thread: Some(db_thread),
+        })
+    }
+
+    /// Fetch the value stored at key.  Will return an error if not found.
+    pub async fn fetch(&self, key: K) -> Result<V, FetchError> {
+        if let Some(val) = self.write_cache.get(&key) {
+            Ok(val.clone())
+        } else {
+            self.db_read.lock().await.fetch(&key)
+        }
+    }
+
+    /// True if the database contains key.
+    pub async fn contains_key(&self, key: K) -> Result<bool, ReadKeyError> {
+        if self.write_cache.contains_key(&key) {
+            Ok(true)
+        } else {
+            self.db_read.lock().await.contains_key(&key)
+        }
+    }
+
+    /// Insert a new key/value pair in Db.
+    /// For the data file this means inserting:
+    ///   - key size (u16) IF it is a variable width key (not needed for fixed width keys)
+    ///   - value size (u32)
+    ///   - key data
+    ///   - value data
+    pub async fn insert(&self, key: K, value: V) {
+        if !self.write_cache.contains_key(&key) {
+            self.write_cache.insert(key.clone(), value);
+            // An error here menas the receiver in the insert thread was closed/dropped.
+            let _ = self.insert_tx.send(InsertCommand::Insert(key)).await;
+        }
+    }
+    /*
+    /// Return the number of records in Db.
+    pub fn len(&self) -> usize {
+        self.inner.borrow().len()
+    }
+
+    /// Is the DB empty?
+    pub fn is_empty(&self) -> bool {
+        self.inner.borrow().is_empty()
+    }
+    */
+
+    /// Flush any caches to disk and sync the data and index file.
+    /// All data should be safely on disk if this call succeeds.
+    pub async fn commit(&self) -> Result<(), CommitError> {
+        let (tx, rx) = tokio::sync::oneshot::channel();
+        // If we can't send then do something drastic (?).
+        if self.insert_tx.send(InsertCommand::Commit(tx)).await.is_ok() {
+            match rx.await {
+                Ok(r) => r,
+                Err(_err) => Err(CommitError::ReceiveFailed)
+            }
+        } else {
+            // An error here means the receiver in the insert thread was dropped/closed.
+            Err(CommitError::SendChannelClosed)
+        }
+    }
+
+    /// Return an iterator over the key values in insertion order.
+    /// Note this iterator only uses the data file not the indexes.
+    pub async fn raw_iter(&self) -> Result<DbRawIter<K, V, KSIZE, S>, LoadHeaderError> {
+        self.db_read.lock().await.raw_iter()
+    }
+
+    fn insert_thread(
+        mut db: DbCore<K, V, KSIZE, S>,
+        db_read: Arc<tokio::sync::Mutex<DbCore<K, V, KSIZE, S>>>,
+        write_cache: Arc<DashMap<K, V>>,
+        mut insert_rx: mpsc::Receiver<InsertCommand<K, KSIZE>>,
+    ) {
+        let mut inserts = Vec::new();
+        let mut done = false;
+        let mut last_insert_error: Option<InsertError> = None;
+        while !done {
+            match insert_rx.blocking_recv() {
+                Some(InsertCommand::Insert(key)) => {
+                    if let Some(value) = write_cache.get(&key) {
+                        match db.insert(key.clone(), &value) {
+                            Err(InsertError::DuplicateKey) => {} // Silently ignore duplicate keys...
+                            Err(err) => last_insert_error = Some(err),
+                            Ok(_) => {}
+                        }
+                    }
+                    inserts.push(key);
+                }
+                Some(InsertCommand::Commit(tx)) => {
+                    if let Some(err) = last_insert_error {
+                        let _ = tx.send(Err(CommitError::PreviousInsertFailed(err)));
+                        last_insert_error = None;
+                    } else {
+                        let mut db_read = db_read.blocking_lock();
+                        let res = db.commit();
+                        db_read.refresh_index();
+                        drop(db_read);
+                        match res {
+                            Ok(()) => {
+                                let _ = tx.send(Ok(()));
+                                for key in inserts.drain(..) {
+                                    write_cache.remove(&key);
+                                }
+                            },
+                            Err(err) => {
+                                let _ = tx.send(Err(err.into()));
+                            },
+                        }
+                    }
+                }
+                Some(InsertCommand::Done) => done = true,
+                None => done = true,  // The sender has been closed or dropped so nothing left to do.
+            }
+        }
+        println!("XXXX Ending db thread");
+    }
+}
+
+/// Error from async commit().
+#[derive(Debug)]
+pub enum CommitError {
+    /// An error flushing any cached data.
+    Flush(FlushError),
+    /// An io error occured syncing the data file.
+    DataFileSync(io::Error),
+    /// An io error occured syncing the index file.
+    IndexFileSync(io::Error),
+    /// If a previous insert() failed return the error.
+    PreviousInsertFailed(InsertError),
+    /// The channel to send commands to the insert db thread has closed (this is fatal for the DB).
+    SendChannelClosed,
+    /// Failed to receive a response, this indicates a failed DB in an unknown state.
+    ReceiveFailed,
+    /// DB is opened read-only.
+    ReadOnly,
+}
+
+impl Error for CommitError {}
+
+impl fmt::Display for CommitError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match &self {
+            Self::Flush(e) => write!(f, "flush: {}", e),
+            Self::DataFileSync(io_err) => write!(f, "data sync: {}", io_err),
+            Self::IndexFileSync(io_err) => write!(f, "index sync: {}", io_err),
+            Self::PreviousInsertFailed(err) => write!(f, "insert failed: {}", err),
+            Self::SendChannelClosed => write!(f, "send channel closed"),
+            Self::ReceiveFailed => write!(f, "receive failed"),
+            Self::ReadOnly => write!(f, "receive failed"),
+        }
+    }
+}
+
+impl From<sldb_core::error::CommitError> for CommitError {
+    fn from(err: sldb_core::error::CommitError) -> Self {
+        match err {
+            sldb_core::error::CommitError::Flush(e) => Self::Flush(e),
+            sldb_core::error::CommitError::DataFileSync(e) => Self::DataFileSync(e),
+            sldb_core::error::CommitError::IndexFileSync(e) => Self::IndexFileSync(e),
+            sldb_core::error::CommitError::ReadOnly => Self::ReadOnly,
+        }
+    }
+}
+
+
+enum InsertCommand<K, const KSIZE: u16>
+where
+    K: Send + Sync + Eq + Hash + DbKey<KSIZE> + DbBytes<K> + Clone + 'static,
+{
+    Insert(K),
+    Commit(tokio::sync::oneshot::Sender<Result<(), CommitError>>),
+    Done,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::time;
+
+    #[tokio::test]
+    async fn test_50k() {
+        let config = DbConfig::new(".", "xxx50k", 1)
+            .no_auto_flush()
+            .create()
+            .truncate(); //.no_write_cache();
+        let db: AsyncDb<u64, String, 8> = AsyncDb::open(config).unwrap();
+        assert!(!db.contains_key(0).await.unwrap());
+        assert!(!db.contains_key(10).await.unwrap());
+        assert!(!db.contains_key(35_000).await.unwrap());
+        assert!(!db.contains_key(49_000).await.unwrap());
+        assert!(!db.contains_key(50_000).await.unwrap());
+
+        let start = time::Instant::now();
+        for i in 0_u64..50_000 {
+            db.insert(i, format!("Value {}", i)).await; //.unwrap();
+                                                        //if i % 500 == 0 {
+            //if i % 11000 == 0 {
+                //db.commit().await.unwrap();
+            //}
+        }
+        println!("XXXX insert time {}", start.elapsed().as_secs_f64());
+        //assert_eq!(db.len(), 50_000);
+        assert!(db.contains_key(0).await.unwrap());
+        assert!(db.contains_key(10).await.unwrap());
+        assert!(db.contains_key(35_000).await.unwrap());
+        assert!(db.contains_key(49_000).await.unwrap());
+        assert!(!db.contains_key(50_000).await.unwrap());
+        let start = time::Instant::now();
+        db.commit().await.unwrap();
+        println!("XXXX commit time {}", start.elapsed().as_secs_f64());
+        let start = time::Instant::now();
+        let vals: Vec<String> = db.raw_iter().await.unwrap().map(|(_k, v)| v).collect();
+        assert_eq!(vals.len(), 50_000);
+        for (i, v) in vals.iter().enumerate() {
+            assert_eq!(v, &format!("Value {}", i));
+        }
+        println!("XXXX iter time {}", start.elapsed().as_secs_f64());
+        let start = time::Instant::now();
+        assert_eq!(&db.fetch(35_000).await.unwrap(), "Value 35000");
+        for i in 0..50_000 {
+            let item = db.fetch(i as u64).await;
+            assert!(item.is_ok(), "Failed on item {}, {:?}", i, item);
+            assert_eq!(&item.unwrap(), &format!("Value {}", i));
+        }
+        println!("XXXX fetch time {}", start.elapsed().as_secs_f64());
+    }
+}
